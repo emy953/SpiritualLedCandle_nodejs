ea1610fe9ed0f5cccd9239a683f5ee079cd68fbc
emy953 <80955889+emy953@users.noreply.github.com>
2025-01-08 12:05:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/startup', async (req, res) => {
           cr: standFound.currency
         });
         await standsRef.doc(standFound.stid).update({ isactive: true });
-        await standsRef.doc(standFound.stid).update({ candlesOn: candlesOn });
+        await standsRef.doc(standFound.stid).update({ candlesOn: parseInt(candlesOn,10) });
       } else {
         const newStandRef = standsRef.doc(stid);
         const newStandData = { ...defaultValues, serialnumber: serialNumber, candlesOn: parseInt(candlesOn,10), stid: stid , totalcandles: parseInt(totalcandles,10)};
